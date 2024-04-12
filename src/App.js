ed51b6556a6fda9869d4754d474e0565178ab627
Borys Ushakov <inlvert@gmail.com>
2024-04-12 20:55:19 +0300

    add reducer counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment": {
+      const newState = {
+        ...state,
+        count: state.count + 1
+      }
+      return newState;
+    }
+    case "decrement": {
+      const newState = {
+        ...state,
+        count: state.count - 1
+      }
+      return newState;
+    }
+    default: return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const hendlerIncrement = () => {
+    const action = {
+      type: "increment"
+    }
+    dispatch(action);
+  }
+
+  const hendlerDecrement = () => {
+    const action = {
+      type: "decrement"
+    }
+    dispatch(action);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header ">
+      <p>Count is: ${state.count}</p>
+      <button onClick={hendlerDecrement}>Decriment</button>
+      <button onClick={hendlerIncrement}>Increment</button>
     </div>
   );
 }
