67652088732cd983cac7a1034ea7c9ce2782313f
Borys Ushakov <inlvert@gmail.com>
2024-04-29 23:21:52 +0300

    refactor redux store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,15 +1,7 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import reducer from "./reducers";
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './sagas'
+import rootReducer from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
 
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(reducer, composeWithDevTools(
-  applyMiddleware(sagaMiddleware)
-));
-
-sagaMiddleware.run(rootSaga);
-
+const store = configureStore({
+  reducer: rootReducer
+});
 export default store;
